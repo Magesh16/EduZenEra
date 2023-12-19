591575b7fda00b26cfbfc51e912902e20f83cf68
Magesh16 <mageshsk16@gmail.com>
2023-12-19 11:13:08 +0530

    Footer in eduzenera

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,14 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
-      
-    </main>
+    <>
+      <section className='bg-dotted-pattern bg-primary-600 bg-contain py-5 md:py-10'>
+        <div className='wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0'>
+          <div className='flex flex-col justify-center gap-8'>
+          <h1 className='text-4xl font-bold '>EduZenEra is the platform which make the minds sharper and bringforth the enthusiasm to all </h1>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='border-t'>
+      <div className='flex-center wrapper flex-between flex flex-col sm:flex-row text-center'>
+      <Link href="/">
+          <Image
+            src="/assets/images/eduzenera.png" width={150} height={80}
+            alt="EduZenEra logo"
+          />
+        </Link>
+        <p className='text-xs'>© 2023 EduZenEra. All rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
